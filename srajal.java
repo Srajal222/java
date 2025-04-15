c267431f82dc61ae6a21ffdd9c7f688dcedcc595
Srajal222 <kavitaraju735@gmail.com>
2025-04-15 03:23:11 -0700

    Add files via upload

--- srajal.java
+++ srajal.java
@@ -1,5 +1,26 @@
 public class srajal{
     public static void main(String[] args) {
-        System.out.println("maa");
+        // System.out.println("maa");
+        // for(int i=1; i<=5; i++){
+        //     for(int j=5; j>=1; j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println(" ");
+        // }
+        for(int i=1; i<=5; i++){
+                 for(int j=5; j>=1; j--){
+                     System.out.print("*");
+                 }
+                 System.out.println(" ");
+            }
+            for(int k=1; k<=1; k++){
+                System.out.print("*");
+            }
+            for(int p=1; p<=1; p++){
+                System.out.print("*");
+            }
+            System.out.println();
+        
+
     }
 }
